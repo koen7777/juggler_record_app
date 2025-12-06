4a48b0701adadb6fd9a3f95eaca2142c38a34213
koen7777 <sanadarokumonnsen@gmail.com>
2025-12-06 20:48:30 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'screens/menu_screen.dart'; // MenuScreen だけ残す
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:html' as html; // Web専用
+import 'dart:ui' as ui; // Web専用
+
+// 既存画面
+import 'screens/menu_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  if (kIsWeb) {
+    // Flutter Web のみ広告タグ登録
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      'adstir-banner',
+      (int viewId) {
+        final container = html.DivElement();
+        container.style.width = '100%';
+        container.style.height = '100%';
+        container.innerHtml = '''
+<script type="text/javascript">
+var adstir_vars = {
+  ver: "4.0",
+  app_id: "MEDIA-d51fb80d",
+  ad_spot: 1,
+  center: false
+};
+</script>
+<script type="text/javascript" src="https://js.ad-stir.com/js/adstir.js"></script>
+''';
+        return container;
+      },
+    );
+  }
+
+  runApp(const MyAppWithBanner());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// MyApp
+class MyAppWithBanner extends StatelessWidget {
+  const MyAppWithBanner({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +48,8 @@ class MyApp extends StatelessWidget {
       title: 'ジャグノート',
       theme: ThemeData(primarySwatch: Colors.green),
 
-      // 最初に表示する画面
-      initialRoute: '/',
-
-      // ルートは MenuScreen のみ
-      routes: {
-        '/': (_) => const MenuScreen(),
-      },
+      // 最初の画面だけ設定
+      home: const AppWithBanner(child: MenuScreen()),
 
       // 日本語対応
       localizationsDelegates: const [
@@ -36,6 +62,34 @@ class MyApp extends StatelessWidget {
         Locale('en'),
       ],
       locale: const Locale('ja'),
+    );
+  }
+}
+
+// 広告付きラッパー（全画面共通）
+class AppWithBanner extends StatelessWidget {
+  final Widget child;
+  const AppWithBanner({required this.child, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // 上部バナー広告
+          SizedBox(
+            height: 100, // SP 320x100 広告用
+            child: kIsWeb
+                ? HtmlElementView(viewType: 'adstir-banner')
+                : Container(
+                    color: Colors.grey.shade300, // Android/iOS向け仮広告スペース
+                    child: const Center(child: Text('広告スペース')),
+                  ),
+          ),
+          // 下の画面は常に全画面表示
+          Expanded(child: child),
+        ],
+      ),
     );
   }
 }
