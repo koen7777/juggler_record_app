6917f41d394db87b652c1964428eac80b272c56f
koen7777 <sanadarokumonnsen@gmail.com>
2025-12-10 20:51:45 +0900

    update web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1303634963"
+    serviceWorkerVersion: "1543895336"
   }
 });
